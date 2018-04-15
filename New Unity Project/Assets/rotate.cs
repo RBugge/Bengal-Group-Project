7e15276010e4cf113c5075a45f63194ffb0d86e2
mchen72 <michellechen720@gmail.com>
2018-04-15 05:28:06 -0400

    Added background music and text
    Button to open keyboard is added in PanoScene but not working. Error notification need to be added

---
+++ New Unity Project/Assets/rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    public GameObject cube;
+    public bool rotation = true;
+
+	// Use this for initialization
+	void Start () {
+        cube = GameObject.Find("Cube");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            rotation = !rotation;
+        }
+
+        if (rotation)
+        {
+            cube.transform.Rotate(0, 100 * Time.deltaTime, 0);
+        }
+	}
+}
